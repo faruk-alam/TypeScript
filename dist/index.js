5180a833a7c73ca8bbd4c097b6acfe629beb0b68
faruk <farukinnfo@gmail.com>
2026-04-22 12:03:06 +0200

    22/04/26 updated

--- dist/index.js
+++ dist/index.js
@@ -48,4 +48,98 @@ let formatGreeting = (name, greeting) => {
 };
 let result = formatGreeting("Rahul", "Hello");
 console.log(result);
+// any type
+let age;
+age = 25;
+age = "twenty five";
+age = true;
+// any time in arrow
+// let things: any[];
+// things = [1,"hello",true,];
+// things.push({name:"Rahul",age:30});
+// function and any type
+let addTwoValues = (value) => {
+    return value + value;
+};
+const result1 = addTwoValues(5);
+console.log(result1);
+//tuples
+// tuple is a fixed length array with specific types for each element
+// tuple is a special type of array that can hold a fixed number of elements of different types.
+let personTuple = ["John", 30, true];
+personTuple = [30, 25, false];
+let hsla;
+hsla = [240, "100%", "50%", 1];
+// tuples in function
+function usecoords() {
+    const lat = 105.55;
+    const long = 78.90;
+    return [lat, long];
+}
+const [latitude, longitude] = usecoords();
+console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+// named tuples
+// named tuples are a way to give names to the elements of a tuple, making it easier to understand the purpose of each element.
+let userInfo;
+userInfo = ["Alice", 28, true];
+console.log(userInfo[0]);
+const authorOne = {
+    name: "John Doe",
+    avatar: "https://example.com/avatar.jpg"
+};
+console.log(authorOne);
+const newPost = {
+    title: "My First Post",
+    body: "This is the content of my first post.",
+    tags: ["typescript", "programming", "webdev"],
+    createdAt: new Date(),
+    author: authorOne
+};
+console.log(newPost);
+// as function argument types
+function createPost(post) {
+    console.log(`created ${newPost.title} by ${newPost.author.name}`);
+}
+createPost(newPost);
+// with array
+let posts = [];
+posts.push(newPost);
+console.log(posts);
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return [r, g, b];
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne, colorTwo);
+const userOne = {
+    name: "Alice",
+    age: 28
+};
+function createUser(user) {
+    console.log(`User ${user.name} is ${user.age} years old.`);
+}
+createUser(userOne);
+// union types
+// union types are a way to define a type that can be one of several types. They are defined using the | operator.
+let someId;
+someId = 123;
+someId = "abc123";
+// someId = true; // error
+let email;
+email = "user@example.com";
+email = null;
+function swapId(id) {
+    if (typeof id === "number") {
+        return id.toString();
+    }
+    else {
+        return parseInt(id);
+    }
+}
+const swappedId1 = swapId(123);
+const swappedId2 = swapId("456");
+console.log(swappedId1, swappedId2);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -61,3 +61,148 @@ let formatGreeting = (name: string, greeting: string) => {
 }
 let result = formatGreeting("Rahul","Hello");
 console.log(result);
+
+// any type
+let age:any;
+
+age = 25;
+age = "twenty five";
+age = true;
+
+// any time in arrow
+// let things: any[];
+// things = [1,"hello",true,];
+// things.push({name:"Rahul",age:30});
+
+// function and any type
+let addTwoValues = (value: any) : any => {
+    return value + value;
+}
+const result1 = addTwoValues(5);
+console.log(result1)
+//tuples
+// tuple is a fixed length array with specific types for each element
+// tuple is a special type of array that can hold a fixed number of elements of different types.
+
+let personTuple : [string,number,boolean] = ["John", 30, true];
+personTuple = [30, 25, false];
+
+let hsla : [number,string,string,number];
+hsla = [240,"100%","50%",1];
+
+// tuples in function
+
+function usecoords() : [number,number] {
+    const lat = 105.55;
+    const long = 78.90;
+    return [lat,long];
+}
+const [latitude,longitude] = usecoords();
+console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+// named tuples
+// named tuples are a way to give names to the elements of a tuple, making it easier to understand the purpose of each element.
+let userInfo : [name: string, age: number, isAdmin: boolean];
+userInfo = ["Alice", 28, true];
+console.log(userInfo[0])
+
+// interface for tuple
+// interfaces in TypeScript are a powerful way to define the structure of an object. They can be used to define the shape of an object, including the types of its properties 
+// and methods. This can help us catch errors at compile time and make our code more maintainable.
+interface Author {
+    name: string;
+    avatar: string;
+}
+const authorOne: Author = {
+    name: "John Doe",
+    avatar: "https://example.com/avatar.jpg"
+}
+console.log(authorOne)
+
+interface Post {
+    title: string;
+    body: string;
+    tags: string[];
+    createdAt: Date;
+    author: Author;
+}
+const newPost: Post = {
+    title: "My First Post",
+    body: "This is the content of my first post.",
+    tags: ["typescript","programming","webdev"],
+    createdAt: new Date(),
+    author: authorOne
+}
+console.log(newPost)
+
+// as function argument types
+function createPost(post: Post) : void {
+   console.log(`created ${newPost.title} by ${newPost.author.name}`);
+}
+createPost(newPost);
+
+
+// with array
+let posts: Post[] = [];
+posts.push(newPost);
+console.log(posts)
+
+
+//type aliases 
+// - type aliases are a way to give a name to a type, making it easier to reuse and understand. They can be used to define complex types, such as union types, 
+// intersection types, and mapped types.
+
+// type aliases - tuple
+type Rgb = [number,number,number]
+function getRandomColor() : Rgb {
+    const r = Math.floor(Math.random()*255)
+    const g = Math.floor(Math.random()*255)
+    const b = Math.floor(Math.random()*255)
+    return [r,g,b]
+}
+const colorOne = getRandomColor();
+const colorTwo = getRandomColor();
+console.log(colorOne,colorTwo)
+
+// type aliase - object literal
+type User = {
+    name: string;
+    age: number;
+}
+const userOne: User = {
+    name: "Alice",
+    age: 28
+}
+
+function createUser(user: User) : void {
+    console.log(`User ${user.name} is ${user.age} years old.`);
+}
+createUser(userOne)
+
+// union types
+// union types are a way to define a type that can be one of several types. They are defined using the | operator.
+
+let someId: number | string;
+someId = 123;
+someId = "abc123";
+// someId = true; // error
+
+let email: string | null;
+email = "user@example.com";
+email = null;
+//email = undefined; // error
+
+// type guards
+// type guards are a way to narrow down the type of a variable within a specific block of code. They are often used in conjunction with union types to determine the actual 
+// type of a variable at runtime.
+type Id = number | string;
+
+function swapId(id: Id) {
+    if (typeof id === "number") {
+        return id.toString();
+    } else {
+        return parseInt(id);
+    }}
+    const swappedId1 = swapId(123);
+    const swappedId2 = swapId("456");
+    console.log(swappedId1,swappedId2)
